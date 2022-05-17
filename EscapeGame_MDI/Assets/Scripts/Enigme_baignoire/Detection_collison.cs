adfad15e30ffa2e32ee5c5bfabdfbea063fdc735
Enora Rolland <96230559+EnoraRolland@users.noreply.github.com>
2022-05-17 16:36:55 +0200

    Debut des scrip pour l'énigme de la baignoire
    Début des scrip pour l'énigme de la baignoire

---
+++ EscapeGame_MDI/Assets/Scripts/Enigme_baignoire/Detection_collison.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection_collison : MonoBehaviour
+{
+    // Bollean de detection
+    public bool isBloodHere = false;
+    public bool isSkullHere = false;
+    public bool isBonesHere = false;
+    public bool isEyeHere = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBloodHere && isSkullHere && isBonesHere && isEyeHere)
+        {
+            //Debug.Log("Gagné");
+        }
+    }
+
+    private void OnCollisionEnter(Collision obj)
+    {
+       if (obj.gameObject.name == "Sang")
+        {
+            Debug.Log("la bouteille de sang est dans la baignoire");
+            isBloodHere = true;
+        }
+        else { isBloodHere = false; }
+
+
+        if (obj.gameObject.name == "Crane")
+        {
+            Debug.Log("le crane est dans la baignoire");
+            isSkullHere = true;
+        }
+       else { isSkullHere = false; }
+
+
+        if (obj.gameObject.name == "Os")
+        {
+            Debug.Log("les os sont dans la baignoire");
+            isBonesHere = true;
+        }
+        else { isBonesHere = false; }
+
+
+        if (obj.gameObject.name == "Yeux")
+        {
+            Debug.Log("les yeux sont dans la baignoire");
+            isEyeHere = true;
+        }
+        else { isEyeHere = false; }
+    }
+}
