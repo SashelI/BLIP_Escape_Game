16e0ecf61db4a96a4e71e1daa24f17654a52f8f7
Bordin Amelie <amelie.bordin@etudiant.univ-rennes1.fr>
2022-05-18 17:22:37 +0200

    Ajout d'un curseur et chgt mineurs dans le script d'inventaire

--- EscapeGame_MDI/Assets/Scripts/Items/Inventairable.cs
+++ EscapeGame_MDI/Assets/Scripts/Items/Inventairable.cs
@@ -7,12 +7,12 @@ public class Inventairable : MonoBehaviour
     public enum ObjectType {cube, key}
     public ObjectType Objtype { get { return type; } set { type = value; } }
     [SerializeField] private ObjectType type;
-    [SerializeField] private Inventory inv;
+    private Inventory inv;
     public string objName;
     // Start is called before the first frame update
     void Start()
     {
-        
+        inv = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
     // Update is called once per frame
